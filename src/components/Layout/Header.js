42cbd0ace26fcd39a8a6c53dd30b03b1deea4303
Maky <franklinjaradev@gmail.com>
2021-06-23 02:07:16 -0400

    HeaderButton and Icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import chidoMealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>Chido Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={chidoMealsImage} alt="Chido Meals - Delicious Food" />
