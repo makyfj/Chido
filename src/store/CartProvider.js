0c32bb0bd5e73cf14314a767fb37c3dbefaf2bf2
Maky <franklinjaradev@gmail.com>
2021-06-23 21:53:31 -0400

    Removing Items from Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -36,6 +36,30 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "REMOVE_CART_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+
+    // decreases the price of one item
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
@@ -48,7 +72,9 @@ const CartProvider = ({ children }) => {
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD_CART_ITEM", item: item });
   };
-  const removeItemFromCartHandler = (item) => {};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
